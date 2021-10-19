10207008bb9b2c2961eae089686eca4f89855f28
sethukoodaranhi <sethukoodaranhi@gmail.com>
2021-10-19 19:56:30 +0530

    initial commit

---
+++ sample.js
@@ -0,0 +1,87 @@
+// string in to uppercase
+
+// var txt=prompt("enter string:");
+// console.log(txt.toUpperCase());
+
+// input text box string
+
+// function upper(){
+//     var txt=document.getElementById("screen").value
+//     var rslt=document.getElementById("ans");
+//     var answer;
+//     answer=txt.toUpperCase();
+//     rslt.innerHTML=answer;
+//     //  document.write(rslt);
+//     console.log(rslt);
+// }
+
+// / pallindrome or not
+
+// var txt=prompt("enter a string");
+// var len;
+// len=txt.length;
+// var pallindrome= true;
+// for(i=0;i<len;i++)
+// {
+//     if(txt[i]==txt[len-1-i])
+//     {
+//         
+//         pallindrome=true;
+//     }
+   
+//     else{
+//        
+//         pallindrome=false;
+//     }
+// }
+// if(pallindrome)
+//     console.log("pallindrome");
+// else
+//     console.log("not pallindrome")
+
+
+// function using pallindrome
+
+// function pallindrome(){
+//     var txt=document.getElementById("screen").value;
+//     var ans=document.getElementById("ans")
+//     var len;
+//     len=txt.length;
+//     var pallindrome=true;
+//     for(i=0;i<len;i++)
+//     {
+//         if(txt[i]==txt[len-1-i])
+//         {
+//             pallindrome=true;
+//         }
+//         else{
+//             pallindrome=false;
+//         }
+//     }
+// if(pallindrome)
+//     ans.innerHTML="pallindrome"
+// else
+//     ans.innerHTML="not pallindrome"
+// }
+
+
+// adding new element into the first of an array
+
+// var flower=["jasmin","rose","sunflower","lotus"]
+// document.write(flower+"<br>");
+// // flower[0]="lilly";
+// flower.unshift("marygold");
+// document.write(flower+"<br>")
+
+
+// add a key value pair in an object(object)
+
+// const person= {
+//     name :'sethu',
+//     age:20,
+//     gender:'female'
+
+// }
+// person.lastname='lakshmi';
+// document.write(person.name + ""+person.lastname);
+// document.write(person);
